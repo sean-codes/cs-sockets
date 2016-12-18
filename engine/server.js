3cf7a4ad9b8d438e8024f2da4b89eed80786091f
sean_codes <sean.mann@alpinehomeair.com>
2016-12-17 19:45:36 -0600

    any size string up to 65xxx

--- engine/server.js
+++ engine/server.js
@@ -21,6 +21,7 @@ const server = http.createServer((requestIncoming, responseOutGoing) => {
 });
 
 server.listen(port, hostname, () => {
+    console.clear();
     console.log(`Server is online on http://${hostname}:${port}`);
 });
 
@@ -31,46 +32,61 @@ server.on('upgrade', (request, socket, head) => {
     //Send Back to requester
     socket.write( 
           'HTTP/1.1 101 Switching Protocols\r\n'
-        + 'Upgrade: WebSocket\r\n'
-        + 'Connection : Upgrade\r\n'
-        + 'Sec-WebSocket-Accept:'+hashedKey+'\r\n' + '\r\n'
+        + 'Upgrade: websocket\r\n'
+        + 'Connection: Upgrade\r\n'
+        + 'Sec-WebSocket-Accept: '+hashedKey+'\r\n'
+        + '\r\n'
     );
 
 
     //Start Keeping an Eye out for Data
     socket.on('data', (data) => {
-        //console.clear();
         console.log('Receiving Message:', data);
         var isFinished = data[0] >> 7;
         var optionCode = data[0] & 15;
         var isMasked = data[1] >> 7;
         var dataLength = data[1] & 127;
+        var dataStart = 6;
+        if(dataLength > 125){
+            if(dataLength == 126){
+                dataLength = data.readUInt16BE(2);
+                dataStart = 8;
+            } else {
+                dataLength = data.readUIntBE(2, 6);
+                dataStart = 12;
+            }
+        }
         console.log('Is Finished: ' + isFinished);
         console.log('Is Masked: ' + isMasked);
         console.log('Option Code: ' + optionCode);
         console.log('Data Length: ' + dataLength);
-        if(optionCode === 1 && isFinished === 1 && isMasked === 1 && dataLength < 126){
-            var maskingKey = data.slice(2, 6);
-            var maskedData = data.slice(6, 6+dataLength);
+        console.log('Data Start: ' + dataStart);
+        if(data[0] === 129 && dataLength < 65535 && optionCode === 1 && isFinished === 1 && isMasked === 1){
+            var maskingKey = data.slice(dataStart-4, dataStart);
+            var maskedData = data.slice(dataStart, dataStart+dataLength);
             var unMaskedData = '';
             for(var i = 0; i < dataLength; i++){
                 unMaskedData += String.fromCharCode(maskedData[i] ^ maskingKey[i % 4]);
             }
-            console.log('unMaskedData: ' + unMaskedData);
             echoTextMessage(socket, unMaskedData);
-        } else {
-            echoTextMessage(socket, 'Message larger than 126 bytes!');
         }
     });
 });
 
-function echoTextMessage(socket, str){
-    var response = [129, str.length];
-    for(var i = 0; i < str.length; i ++){
-        response.push(str.charCodeAt(i));
-    }
-    console.log(response);
-    socket.write(new Buffer(response));
+function echoTextMessage(socket, str) { 
+    if(str.length < 125) {
+        var dataOffset = 2;
+        var response = Buffer.allocUnsafe(dataOffset+str.length);
+        response.writeUInt8(129, 0);
+        response.writeUInt8(str.length, 1);
+    } else {
+        var dataOffset = 4;
+        var response = Buffer.allocUnsafe(dataOffset+str.length);
+        response.writeUInt8(129, 0);
+        response.writeUInt8(126, 1);
+        response.writeUInt16BE(str.length, 2);
+    } 
+    response.write(str, dataOffset);
+    socket.write(response);
 }
-
 
