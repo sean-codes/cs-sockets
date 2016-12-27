097b9a1d9b7086ba0a9bc6a469fd231d9bea8e10
sean_codes <sean.mann@alpinehomeair.com>
2016-12-26 23:04:37 -0600

    [UPDATE] - Templating sending messages

--- engine/server.js
+++ engine/server.js
@@ -1,9 +1,15 @@
 //Require Build In
-const http = require('http');
-const fs = require('fs');
 const csServer = require('./csServer');
 
-//Sending a Message
 csServer.on('message', function(socket, message){
-    csServer.send(socket, message);
-})
+	//To a Specific SocketID
+    socket.message(socket, message);
+    //To everyone but SocketID
+    //socket.send('multicast', message);
+
+    //To Everyone in Room
+    //socket.send('broadcast', message);
+
+    //To Everyone in Game
+    //socket.send('globalcast', message);
+});
