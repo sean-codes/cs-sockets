bee3d22af4f90716405fbbea05baa4a2603c8dec
sean_codes <sean.mann@alpinehomeair.com>
2016-12-26 20:05:41 -0600

    [UPDATE] - Socket does not handle sending/framing

--- engine/csSocket.js
+++ engine/csSocket.js
@@ -1,9 +1,3 @@
-//Receiving Data States
-const STATE_START = 0;
-const STATE_GET_LENGTH = 1;
-const STATE_GET_MASK = 2;
-const STATE_GET_DATA = 3;
-
 //Finished/Opcode Values
 const FO_UNFINISHED = 1;
 const FO_CONTINUATION = 128;
@@ -11,18 +5,27 @@ const FO_FINISHED = 129;
 const FO_END = 0;
 const FO_PING = 0;
 
-//Payload
+//Receiving Data States
+const STATE_START = 0;
+const STATE_GET_LENGTH = 1;
+const STATE_GET_MASK = 2;
+const STATE_GET_DATA = 3;
+
+
+//Payload/Buffer
 const PL_LARGE = 126;
+const EMPTY_BUFFER = Buffer.allocUnsafe(0);
 
 class csSocket {
-    constructor (socket) {
+    constructor (server, socket) {
         this.socket = socket;
-        this.buffer = Buffer.allocUnsafe(0);
+        this.server = server;
+        this.buffer = EMPTY_BUFFER;
         this.state  = STATE_START;
         this.payloadLength = 0;
         this.payloadStart = 0;
         this.cont = false;
-        this.continuationBuffer = Buffer.allocUnsafe(0);
+        this.continuationBuffer = EMPTY_BUFFER;
         this.finished = true;
     }
 
@@ -45,6 +48,7 @@ class csSocket {
 
     start(newDataLength){
         if(this.buffer.length < 2) return;
+        newDataLength -= 2;
         this.maskOpBlah = this.bufferRead(1)[0];
         if(this.maskOpBlah !== FO_FINISHED){
             if(this.maskOpBlah == FO_UNFINISHED){
@@ -68,6 +72,7 @@ class csSocket {
 
     getLength(newDataLength){
         if(this.buffer.length < 2) return
+        newDataLength -= 2;
         this.payloadLength = this.bufferRead(2).readUInt16BE(0);
         this.state = STATE_GET_MASK;
         this.getMask(newDataLength);
@@ -75,6 +80,7 @@ class csSocket {
 
     getMask(newDataLength){
         if(this.buffer.length < 4) return
+        newDataLength -= 4;
         this.mask = this.bufferRead(4);
         this.state = STATE_GET_DATA;
         this.getData(newDataLength);
@@ -95,36 +101,21 @@ class csSocket {
 
             //Write back or save for later
             if(this.finished === true){
-                var responseLength = this.frameDataAndSend(response);
+                this.server.emit('message', this.socket, Buffer.concat([this.continuationBuffer, response]).toString());
+                this.continuationBuffer = EMPTY_BUFFER;
                 //call me onmessage function
             } else {                
                 this.continuationBuffer = Buffer.concat([this.continuationBuffer, response]);
-                var responseLength =  response.length
             }
             
             this.state = STATE_START;
-            newDataLength -= responseLength;
+
+            //Response Length
+            newDataLength -= response.length;
             if(newDataLength > 0){
                 this.start(newDataLength);
             }
         }
-    }
-
-    frameDataAndSend(data){
-        if(data.length + this.continuationBuffer.length < PL_LARGE){
-            var header = Buffer.allocUnsafe(2);
-            header.writeUInt8(FO_FINISHED, 0);
-            header.writeUInt8(data.length+this.continuationBuffer.length, 1);
-        } else {
-            var header = Buffer.allocUnsafe(4);
-            header.writeUInt8(FO_FINISHED, 0);
-            header.writeUInt8(PL_LARGE, 1);
-            header.writeUInt16BE(data.length+this.continuationBuffer.length, 2);
-        }
-        var test = Buffer.concat([header, this.continuationBuffer, data]);
-        this.socket.write(test);
-        this.continuationBuffer = Buffer.allocUnsafe(0);
-        return test.length;
     }
 
     bufferRead(cnt){
@@ -136,21 +127,23 @@ class csSocket {
         return read;
     }
 
-    sendMessage(str) { 
-        if(str.length < PL_LARGE) {
-            var dataOffset = 2;
-            var response = Buffer.allocUnsafe(dataOffset+str.length);
-            response.writeUInt8(FO_FINISHED, 0);
-            response.writeUInt8(str.length, 1);
+    frameDataAndSend(data) { 
+        if(data.length + this.continuationBuffer.length < PL_LARGE){
+            var header = Buffer.allocUnsafe(2);
+            header.writeUInt8(FO_FINISHED, 0);
+            header.writeUInt8(data.length+this.continuationBuffer.length, 1);
         } else {
-            var dataOffset = 4;
-            var response = Buffer.allocUnsafe(dataOffset+str.length);
-            response.writeUInt8(FO_FINISHED, 0);
-            response.writeUInt8(PL_LARGE, 1);
-            response.writeUInt16BE(str.length, 2);
-        } 
-        response.write(str, dataOffset);
-        this.write(response);
+            var header = Buffer.allocUnsafe(4);
+            header.writeUInt8(FO_FINISHED, 0);
+            header.writeUInt8(PL_LARGE, 1);
+            header.writeUInt16BE(data.length+this.continuationBuffer.length, 2);
+        }
+        var test = Buffer.concat([header, this.continuationBuffer, data]);
+        if(typeof this.server._events.message == 'function'){
+            this.server.emit('message', this.socket, test);
+        }
+        this.continuationBuffer = Buffer.allocUnsafe(0);
+        return test.length;
     }
 }
 
