052755e5f1e0a8402ded68c4e9cba62a9e50c6c0
sean_codes <sean.mann@alpinehomeair.com>
2016-12-30 07:10:14 -0600

    [UPDATE] - creating csServer object

--- engine/server.js
+++ engine/server.js
@@ -1,17 +1,13 @@
 //Require Build In
 const csServer = require('./csServer');
-csServer.on('open', function(client){
-	//Add to list
-	console.log('real open');
+const server = new csServer('127.0.0.1', '9999', 'index.html');
+
+server.on('open', function(client){
 	client.on('message', function(message){
 		this.sendMessage(this, message);
-		//this.sendMessage('globalcast', message);
-		//this.sendMessage('broadcast', message);
-		//this.sendMessage('multicast', message);
 	});
 
 	client.on('close', function(){
-		//Remove from list
-		console.log('emitted close');
+		console.log('Client Connection Closed');
 	});
 });
