1d4d72d8557624113023854a902e3baab4638370
sean_codes <sean.mann@alpinehomeair.com>
2016-12-30 04:01:32 -0600

    [UPDATE] - Emit close for socket

--- engine/csServer.js
+++ engine/csServer.js
@@ -30,25 +30,6 @@ csServer.listen(PORT, HOSTNAME, () => {
     console.log(`Server is online on http://${HOSTNAME}:${PORT}`);
 });
 
-//Holding Connections
-csServer.clientList = [];
-csServer.addClient = function(socket){
-    console.log('Client Added!');
-    var id = this.clientList.length;
-    this.clientList.push({
-        id:     id,
-        room:   0,
-        socket: socket
-    });
-    return this.clientList[id];
-}
-
-csServer.removeClient = function(socket){
-    console.log('Client Removed!');
-    socket.id = -1;//Dead Socket
-    csServer.clientList.pop(socket.id);
-}
-
 //Handle Upgrades to Websocket
 csServer.on('upgrade', function(request, socket, head){
     var secWebSocketKey = request.headers['sec-websocket-key'] + WS_MAGIC_STRING;
@@ -61,10 +42,10 @@ csServer.on('upgrade', function(request, socket, head){
         + `Sec-WebSocket-Accept: ${hashedKey}\r\n\r\n`
     );
     socket.setTimeout(0);
-    //socket.allowHalfOpen = false;
+    socket.allowHalfOpen = false;
     socket.setNoDelay(true);
-    socket.cs = new csSocket(this, socket, csServer.addClient(socket)); 
-    socket.kill = false;
+    socket.cs = new csSocket(this, socket); 
+    this.emit('open', socket.cs);
     //Basic Event Handling
     socket.on('data', function(newData){
         if(socket.id == -1) return;
@@ -74,59 +55,17 @@ csServer.on('upgrade', function(request, socket, head){
 
     socket.on('close', function(){
         console.log('Socket Closed');
-        csServer.removeClient(this);
+        this.cs.emit('close'); 
     });
 
     socket.on('end', function(){
-        console.log('Socket Ended');
+        console.log('Socket End Then running destroy');
+        this.destroy();
     });
 
     socket.on('error', function(){
         console.log('Socket Error');
     })
 });
-
-csServer.sendMessage = function(from, to, data){
-    if(typeof to == 'string'){
-        for(var i = 0; i < this.clientList.length; i++){
-            var client = this.clientList[i];
-            switch(to){
-                case 'multicast':
-                    if(i !== from.id && client.room == from.room){
-                        csServer.frameAndSendData(client, data);
-                    }
-                    break;
-                case 'broadcast':
-                    if(client.room == from.room){
-                        csServer.frameAndSendData(client, data);
-                    }
-                    break;
-                case 'globalcast':
-                    csServer.frameAndSendData(client, data);
-                    break;
-            }
-        }
-    } else {
-        csServer.frameAndSendData(to, data);
-    }
-    
-}
-
-csServer.frameAndSendData = function(client, data){
-    //Sending Data
-    if(data.length < PL_LARGE){
-        var header = Buffer.allocUnsafe(2);
-        header.writeUInt8(FO_FINISHED, 0);
-        header.writeUInt8(data.length, 1);
-    } else {
-        var header = Buffer.allocUnsafe(4);
-        header.writeUInt8(FO_FINISHED, 0);
-        header.writeUInt8(PL_LARGE, 1);
-        header.writeUInt16BE(data.length, 2);
-    }
-    var bufferData = Buffer.from(data);
-    var headerWithData = Buffer.concat([header, bufferData]);
-    client.socket.write(headerWithData);
-}
 
 module.exports = csServer;
