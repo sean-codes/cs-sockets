5d40632abb1ce0593e125a99fa4373a3ca2cbe28
sean_codes <sean.mann@alpinehomeair.com>
2016-12-31 21:07:42 -0600

    [UPDATE] - Connect, message, close

--- engine/server.js
+++ engine/server.js
@@ -2,7 +2,10 @@
 const csServer = require('./csServer');
 const server = new csServer('127.0.0.1', '9999', 'index.html');
 
-server.on('open', function(client){
+server.on('connect', function(client){
+	//Client Connecting
+	console.log('Client Connected');
+	
 	client.on('message', function(message){
 		this.sendMessage(this, message);
 	});
