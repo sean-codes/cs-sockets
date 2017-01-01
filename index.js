422d8ccc1278b74c755c40003b7af2fa001e5935
sean_codes <sean.mann@alpinehomeair.com>
2016-12-31 22:26:30 -0600

    [UPDATE] - type

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * CS-Sockets: Server for text based websocketing
+ * Copyright(c) 2017 Sean Mann <sean_codes@outlook.com>
+ * MIT Licensed
+ */
+
+
+server = require('./engine/csServer.js');
+module.exports = server;
