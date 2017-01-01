13bc7ff8ad634c33c73643e1cd8182e974b66028
sean_codes <sean.mann@alpinehomeair.com>
2016-12-31 21:04:16 -0600

    [UPDATE] - remove comments

--- engine/csServer.js
+++ engine/csServer.js
@@ -49,7 +49,7 @@ class csServer extends Emitter{
             socket.setNoDelay(true);
             socket.cs = new csSocket(this, socket); 
 
-            this.csServer.emit('open', socket.cs);
+            this.csServer.emit('connect', socket.cs);
             //Basic Event Handling
             socket.on('data', function(newData){
                 if(socket.id == -1) return;
@@ -58,75 +58,14 @@ class csServer extends Emitter{
             }); 
 
             socket.on('close', function(){
-                console.log('Socket Closed');
                 this.cs.emit('close'); 
             });
 
             socket.on('end', function(){
-                console.log('Socket End Then running destroy');
                 this.destroy();
-            });
-
-            socket.on('error', function(){
-                console.log('Socket Error');
             });
         });
     }
 }
 
-//Create Server
-/*
-csServer = http.createServer((requestIncoming, responseOutGoing) => {
-    responseOutGoing.statusCode = 200;
-    responseOutGoing.setHeader('Contenet-Type', 'text/html');
-
-    fs.readFile(HTML_INDEX, function(error, content){ 
-        responseOutGoing.end(content);
-    });    
-});
-    
-
-//Listen
-csServer.listen(PORT, HOSTNAME, () => {
-    console.log(`Server is online on http://${HOSTNAME}:${PORT}`);
-});
-
-//Handle Upgrades to Websocket
-csServer.on('upgrade', function(request, socket, head){
-    var secWebSocketKey = request.headers['sec-websocket-key'] + WS_MAGIC_STRING;
-    var hashedKey = crypto.createHash('SHA1').update(secWebSocketKey).digest('base64');
-
-    socket.write( 
-          'HTTP/1.1 101 Switching Protocols\r\n'
-        + 'Upgrade: websocket\r\n'
-        + 'Connection: Upgrade\r\n'
-        + `Sec-WebSocket-Accept: ${hashedKey}\r\n\r\n`
-    );
-    socket.setTimeout(0);
-    socket.allowHalfOpen = false;
-    socket.setNoDelay(true);
-    socket.cs = new csSocket(this, socket); 
-    this.emit('open', socket.cs);
-    //Basic Event Handling
-    socket.on('data', function(newData){
-        if(socket.id == -1) return;
-        this.cs.buffer = Buffer.concat([socket.cs.buffer, newData]);
-        this.cs.receivedData(newData.length);
-    }); 
-
-    socket.on('close', function(){
-        console.log('Socket Closed');
-        this.cs.emit('close'); 
-    });
-
-    socket.on('end', function(){
-        console.log('Socket End Then running destroy');
-        this.destroy();
-    });
-
-    socket.on('error', function(){
-        console.log('Socket Error');
-    })
-});
-*/
 module.exports = csServer;
